0d6634cdd905fba67e668aee8c2b0a9a06c7a97c
Aditya @ WSL-Ubuntu <adityapi3.14@gmail.com>
2023-09-22 21:31:04 +0530

    update: Is Subsequence, 22nd September, 2023.

---
+++ code/001_is_subsequence.cpp
@@ -0,0 +1,31 @@
+/* 22nd September, 2023
+ * Problem: Is Subsequence ( https://leetcode.com/problems/is-subsequence/description/ )
+ * Difficulty: Easy
+ * Solution: Iterate through 's' and greedily find the matching character in 't'.
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+    public:
+        bool isSubsequence(string s, string t){
+            int n = s.length(), m = t.length();
+            bool ans = true;
+            int j = 0;
+            for (int i = 0; i < n; i++){
+                while((j < m) && (s[i] != t[j])) j++;
+                if (j == m) ans = false;
+                j++;
+            }
+
+            return ans;
+        }
+};
+
+int main(){
+    Solution *solver = new Solution;
+    cout << solver->isSubsequence("abc", "ahdfdbdfac") << "\n";
+    exit(EXIT_SUCCESS);
+}
