a28173f671ce301661f5d39c625d1cf06ccad342
Aditya @ WSL-Ubuntu <adityapi3.14@gmail.com>
2023-09-27 08:12:31 +0530

    update: Decode String at Index, 27th September, 2023.

---
+++ code/006_decoded_string_at_index.cpp
@@ -0,0 +1,40 @@
+/* 26th September, 2023
+ * Problem: Decoded String at Index ()
+ * Difficulty: Medium
+ * Solution: 
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+    public:
+        string solve(string &s, int k, vector<long long> &strlen, vector<bool> &is_letter){
+            int lb = lower_bound(strlen.begin(), strlen.end(), k) - strlen.begin();
+            if (is_letter[lb]) return string(1, s[lb]);
+            return solve(s, ((k-1) % strlen[lb-1]) + 1, strlen, is_letter);
+        }
+
+        string decodeAtIndex(string s, int k){
+            int n = s.length();
+            vector<long long> strlen(n);
+            vector<bool> is_letter(n, true);
+            strlen[0] = 1;
+            for (int i = 1; i < n; i++){
+                if ((s[i] >= '0') && (s[i] <= '9')){
+                    strlen[i] = strlen[i-1] * (s[i]-'0');
+                    is_letter[i] = false;
+                }
+                else strlen[i] = strlen[i-1] + 1;
+            }
+
+            return solve(s, k, strlen, is_letter);
+        }
+};
+
+int main(){
+    Solution solver;
+    cout << solver.decodeAtIndex("a2b3c4d5e6f7g8h9", 9) << "\n";
+    exit(EXIT_SUCCESS);
+}
